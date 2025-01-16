d141b26f2ab862cdb4673fbf41a6ad5f69c52c6b
PauloSanRibeiro <paulo_pr11@hotmail.com>
2025-01-15 22:45:24 -0300

    Starting Project and creating client entity

---
+++ src/test/java/br/com/prsr/challengecrud/ChallengecrudApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.prsr.challengecrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengecrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
